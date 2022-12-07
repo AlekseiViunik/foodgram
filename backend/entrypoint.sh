3d6104eb8a3689e6152d383c027892448f825eaf
GoodLifeSeeker <vjunik88@gmail.com>
2022-12-07 23:02:59 +0300

    fix entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,5 @@
 pip3 install -r /app/requirements.txt --no-cache-dir
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn foodgram.wsgi:application --bind 0:8000
+gunicorn backend.wsgi:application --bind 0:8000
 exec "$@"
