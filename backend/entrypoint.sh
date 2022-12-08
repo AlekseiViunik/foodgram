cf358608bf273723b0d2096a5de2197b55fc7cee
GoodLifeSeeker <vjunik88@gmail.com>
2022-12-08 14:08:10 +0300

    added makemigrations to entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 pip3 install -r /app/requirements.txt --no-cache-dir
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn backend.wsgi:application --bind 0:8000
